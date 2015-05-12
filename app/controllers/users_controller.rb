090bc79c9bf624327e872efe8992d2cfa81e00e7
Liann Sun <lisunshiny@gmail.com>
2015-05-12 17:59:50 -0400

    add password confirmation to user save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save(@user)
+    if passwords_match? && @user.save(@user)
       login_user!(@user)
       redirect_to root_url
     else
-      flash.now[:errors] = @user.errors.full_messages
+      flash.now[:errors] ||= []
+      flash.now[:errors].concat(@user.errors.full_messages)
+
       render :new
     end
   end
@@ -27,6 +29,15 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def passwords_match?
+    unless params[:user][:password_confirmation] == user_params[:password]
+      flash.now[:errors] = ["Passwords don't match"]
+      return false
+    end
+
+    return true
   end
 
 end
