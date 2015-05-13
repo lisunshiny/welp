e47175473a832a72e3f25e16fb87074cad5f2ccd
Liann Sun <lisunshiny@gmail.com>
2015-05-13 18:11:47 -0400

    add restaurant form partial

--- app/assets/javascripts/views/restaurant_form.js
+++ app/assets/javascripts/views/restaurant_form.js
@@ -1,3 +1,40 @@
 Welp.Views.RestaurantForm = Backbone.View.extend({
-  
+  tagName: "form",
+
+  className: "restaurant-form",
+
+  events: {
+    "click .submit-restaurant": "submit"
+  },
+
+  initialize: function (opts) {
+    // this.listenTo(this.model, "sync change", this.render);
+  },
+
+  template: JST["restaurants/form"],
+
+  render: function() {
+    var content = this.template();
+    this.$el.html(content);
+
+    return this;
+  },
+
+  submit: function() {
+    event.preventDefault();
+    var attrs = this.$el.serializeJSON();
+    var that = this;
+
+    this.model.save(attrs, {
+      success: function() {
+        that.collection.add(that.model);
+        Backbone.history.loadUrl();
+      },
+
+      error: function(model, response) {
+        debugger;
+        console.log(response);
+      }
+    });
+  }
 })
