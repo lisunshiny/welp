57343c511381fec9fbaaeba0974319a25c1bc242
Liann Sun <lisunshiny@gmail.com>
2015-05-20 10:56:16 -0400

    refactor map index view

--- app/assets/javascripts/views/maps/index_map.js
+++ app/assets/javascripts/views/maps/index_map.js
@@ -4,20 +4,34 @@ Welp.Views.IndexMap = Backbone.View.extend({
     class: "index-map-canvas"
   },
 
-  initialize: function() {
+  initialize: function(opts) {
+    if (typeof opts.center === "undefined") {
+      this.center = { lat: 40.724948, lng: -73.9967097 }
+    }
+    else {
+      this.center = opts.center;
+
+    }
+
     this.listenTo(this.collection, "sync", this.addMarkers)
   },
 
   initMap: function () {
-    // Call this method `render` if you like; I've changed the name so users
-    // need to be deliberate about calling it. The important part is that the
-    // map object should only be instantiated ONCE for a given MapShow view.
+
     var mapOptions = {
-      center: { lat: 40.724948, lng: -73.9967097 },
-      zoom: 12
+      center: this.center,
+      zoom: 12,
     };
 
     this._map = new google.maps.Map(this.el, mapOptions);
+
+    //set a marker at the center of the screen
+    var centerMarker = new google.maps.Marker({
+      position: this.center,
+      map: this._map,
+      zIndex: -1
+    })
+
     this.addMarkers();
   },
 
@@ -30,7 +44,12 @@ Welp.Views.IndexMap = Backbone.View.extend({
 
     var that = this;
 
-    this.collection.markers().each(function(marker) {
+    this.collection.markers().each(function(marker, ord) {
+      var url = "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld="
+        + (ord + 1)
+        + "|FF0000|FFFFFF"
+
+      marker.setIcon(url);
       marker.setMap(that._map);
 
     })
